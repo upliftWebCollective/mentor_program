0ce187cca23d0cba9fccb115d4c495567db122e5
Rawradin <edgargdev@gmail.com>
2017-02-21 22:32:30 -0800

    add additional fields to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170222053319) do
+ActiveRecord::Schema.define(version: 20170222060540) do
 
   create_table "mentee_tags", force: :cascade do |t|
     t.integer  "mentee_id"
@@ -53,6 +53,10 @@ ActiveRecord::Schema.define(version: 20170222053319) do
     t.datetime "created_at",                                null: false
     t.datetime "updated_at",                                null: false
     t.string   "type",                   default: "Mentee"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "twitter_link"
+    t.string   "github_link"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
